3da2b56f9a3c021c49e119b7d79b19a20139359c
Andrii Balaban <andrii.balaban@dfsco.com>
2017-12-22 17:19:13 +0200

    Add World type

---
+++ GameOfLife/World.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    public class World
+    {
+        private readonly int _heightInUnits;
+        private readonly int _widthInUnits;
+
+        public World(int heightInUnits, int widthInUnits)
+        {
+            if (heightInUnits < 100)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(heightInUnits)} should be greater then 100 units");
+            }
+
+            if (_widthInUnits < 100)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(widthInUnits)} should be greater then 100 units");
+            }
+
+            _heightInUnits = heightInUnits;
+            _widthInUnits = widthInUnits;
+        }
+    }
+}
