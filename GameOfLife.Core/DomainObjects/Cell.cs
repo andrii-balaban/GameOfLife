69b862b775d156a5bbcc8d5105843740b88f2d2a
Andrii Balaban <andrii.balaban@dfsco.com>
2017-12-22 18:06:43 +0200

    Add Cell type

---
+++ GameOfLife.Core/DomainObjects/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Core.DomainObjects
+{
+    public class Cell
+    {
+        private int _cellSize;
+
+        public Cell(int cellSize = 2)
+        {
+            _cellSize = cellSize;
+        }
+    }
+}
---
+++ GameOfLife.UnitTests/CellTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using GameOfLife.Core.DomainObjects;
+using NUnit.Framework;
+
+namespace GameOfLife.UnitTests
+{
+    [TestFixture]
+    public class CellTests
+    {
+        [Test]
+        public void Ctor_ShouldNotThrowException()
+        {
+            // Arrange
+
+            // Act
+            Action act = () => new Cell();
+
+            // Assert
+            act.ShouldNotThrow();
+        }
+    }
+}
